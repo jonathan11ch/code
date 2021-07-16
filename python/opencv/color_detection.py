de55f2b27cbb26fe591022b3c57c28b57e3db654
jonathan11ch <jonathan11ch@hotmail.com>
2021-07-15 21:47:12 -0400

    edge detection

--- python/opencv/color_detection.py
+++ python/opencv/color_detection.py
@@ -5,7 +5,7 @@ def createTrackBarWindow():
 	#create empty window
 	cv2.namedWindow("TrackBars")
 	#resize window
-	cv2.resizeWindow("TrackBars",1200,240)
+	cv2.resizeWindow("TrackBars",540,240)
 	#create sliders to set Hue, Saturation, Value
 	cv2.createTrackbar("Hue min","TrackBars", 0  ,179,onChange)
 	cv2.createTrackbar("Hue max","TrackBars", 179,179,onChange)
@@ -18,24 +18,37 @@ def createTrackBarWindow():
 def onChange(a):
 	pass
 
+def getTrackbarValues():
+	hue_min = cv2.getTrackbarPos("Hue min","TrackBars")
+	hue_max = cv2.getTrackbarPos("Hue max","TrackBars")
+	sat_min = cv2.getTrackbarPos("Sat min","TrackBars")
+	sat_max = cv2.getTrackbarPos("Sat max","TrackBars")
+	val_min = cv2.getTrackbarPos("Val min","TrackBars")
+	val_max = cv2.getTrackbarPos("Val max","TrackBars")
+	return [hue_min,hue_max,sat_min,sat_max,val_min,val_max]
 
 def main():
 	#load image
-	img =  cv2.imread("fig/fig7.jpg")
-
+	img =  cv2.imread("fig/fig8.jpg")
+	img = cv2.resize(img,(int(img.shape[1]/4),int(img.shape[0]/4)))
 	#create trackbar
 	createTrackBarWindow()
 	#convert to HSV color space
 	imgHSV = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 	#show image
 	while True:
-		hue_min = cv2.getTrackbarPos("Hue min","TrackBars")
-		#cv2.imshow("image", img)
-		cv2.imshow("image HSV", imgHSV)
-		print(hue_min)
+
+		[hue_min,hue_max,sat_min,sat_max,val_min,val_max] = getTrackbarValues()
+		#set color bounds
+		lower = np.array([hue_min,sat_min,val_min])
+		upper = np.array([hue_max,sat_max,val_max])
+		#create mask
+		mask  = cv2.inRange(imgHSV,lower,upper)
+		#apply mask to the original image (AND) operation
+		imgResult = cv2.bitwise_and(img, img, mask=mask)
+		#show image
+		cv2.imshow("image HSV", imgResult)
 		cv2.waitKey(100)
 
-	
 
 main()
-
---
+++ python/opencv/edge_detection.py
@@ -0,0 +1,43 @@
+import cv2
+import numpy as np
+
+
+def getContours(img):
+    contours,hierarchy = cv2.findContours(img,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        #print(area)
+        if area > 500:
+            perimeter = cv2.arcLength(cnt, True)
+            #print(perimeter)
+            approx = cv2.approxPolyDP(cnt, 0.02*perimeter, True)
+            if len(approx) == 6:
+                objectType = "Hex"
+                cv2.drawContours(imgContour, cnt, -1,(0,255,0),3)
+                x,y,w,h = cv2.boundingRect(approx)
+                cv2.rectangle(imgContour, (x,y),(x+w,y+h),(0,0,255),4)
+                cv2.putText(imgContour,objectType,(x+(w//2)-10, y+(h//2 -10)),cv2.FONT_HERSHEY_COMPLEX,0.5,(0,0,0),2)
+
+
+
+
+img     = cv2.imread("fig/shapes.jpg")
+img = cv2.resize(img, (int(img.shape[1]/2),int(img.shape[0]/2)))
+
+
+imgGray  = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+imgBlur  = cv2.GaussianBlur(imgGray,(7,7),1)
+imgCanny = cv2.Canny(imgBlur,50,50)
+imgContour = img.copy()
+getContours(imgCanny)
+
+#stack images
+imgBlank = np.zeros_like(imgGray)
+imgStack1 = np.hstack((imgBlur,imgGray))
+imgstack2 = np.hstack((imgCanny,imgBlank))
+
+imgStack =  np.vstack((imgStack1,imgstack2))
+
+cv2.imshow("figure", imgStack)
+cv2.imshow("figure2", imgContour)
+cv2.waitKey(0)
